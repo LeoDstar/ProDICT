58ea3ce7935f9c0d43a11c45eadadecca2c1b902
LeoDstar <leonardo.destrada29@gmail.com>
2025-07-20 12:34:40 +0200

    function for protein filter basedd on peptides

--- src/data/feature_selection.py
+++ src/data/feature_selection.py
@@ -26,3 +26,26 @@ def binary_labeling(df: pd.DataFrame, classified_by: str, true_class: list ) -> 
 
 
     return df
+
+
+def get_high_confidence_proteins(peptide_binary_df:pd.DataFrame, true_class:list, classified_by:str, threshold=0.7) -> list[str] :
+    """
+    Returns a list of protein names with identification in 70% of samples with at leat 2 peptides.
+     
+    Args:
+        peptide_binary_df (pd.DataFrame): DataFrame with binary peptide data.
+        true_class (list): List of classes to filter the DataFrame.
+        classified_by (str): Column name used for classification.
+        threshold (float): Minimum percentage of samples in which a protein must be identified to be included
+    
+    Returns:
+        list[str]: List of protein names that meet the criteria.
+    """
+    class_peptides = peptide_binary_df[peptide_binary_df[classified_by].isin(true_class)]
+    class_peptides_stats = class_peptides.describe()
+    proteins_by_peptides = list(
+        class_peptides_stats.T[class_peptides_stats.T['mean'] >= threshold].index
+    )
+    print (f" {len(proteins_by_peptides)} proteins identified in {threshold*100}% of {true_class} samples")
+    
+    return proteins_by_peptides
