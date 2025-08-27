f8edf7b3600c9263b93fd753972161e12cef7645
LeoDstar <leonardo.destrada29@gmail.com>
2025-08-27 23:40:39 +0200

    scripts for LML3; HP tunning

--- src/data/entity_model_settings_COAD_READ.py
+++ src/data/entity_model_settings_COAD_READ.py
@@ -15,7 +15,7 @@ PREPROCESSED_FP_Z_SCORES = 'full_proteome_measures_z.tsv'
 METADATA_FILE = 'METADATA_PANCANCER_PAPER_final.xlsx'
 
 # Classification parameters
-TARGET_CLASS = ['MPNST']  # Change this for different tumor types (e.g., ['BRCA'], ['LUAD'], etc.)
+TARGET_CLASS = ['COAD', 'READ']  # Change this for different tumor types (e.g., ['BRCA'], ['LUAD'], etc.)
 CLASSIFIED_BY = 'code_oncotree'  # Column name for classification - default is 'code_oncotree'
 SAMPLES_COLUMN = 'Sample name'  # Column name for sample identifiers - default is 'Sample name'
 CELL_CONTENT_COLUMN = 'TCC_Bioinfo'  # Column name for tumor cell content - default is 'TCC_Bioinfo'
@@ -28,14 +28,14 @@ HIGH_CONFIDENCE_THRESHOLD = 0.7  # Threshold for high confidence proteins
 
 # Feature selection parameters
 FEATURE_SELECTION_L1_RATIOS = [0.3,0.5]  # L1 ratios to test for ElasticNet
-FEATURE_SELECTION_C_VALUES = [2, 3, 4]     # C values to test for ElasticNet
+FEATURE_SELECTION_C_VALUES = [1.5,3,5]     # C values to test for ElasticNet
 GRID_SEARCH_N_SPLITS = 3           # Number of splits for grid search
 
 ELNET_L1_RATIO = 0.5                     # fallback value L1 ratio for ElasticNet hyperparameter selection
 ELNET_C_VALUE = 1                        # fallback value C value for ElasticNet hyperparameter selection
 ELNET_N_SPLITS = 3                       # Number of splits for cross-validation
 ELNET_N_REPEATS = 67                     # Number of repeats for cross-validation
-ELNET_N_JOBS = 16                        # Number of parallel jobs
+ELNET_N_JOBS = 12                        # Number of parallel jobs
 
 # Model fitting parameters
 NESTED_CV_RANDOM_STATE_TRIES = 10    # Number of random state tries for nested CV
